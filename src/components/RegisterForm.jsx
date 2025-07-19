f44925ccad63d7213d857966bbaed6f42c5a0207
vinayg2027 <vinayakumara.gajula@gmail.com>
2025-07-19 11:27:31 +0530

    new p tag added

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
 
 const RegisterForm = () => {
   return (
     <div>
       <h2>Register</h2>
+      <p> this is testing paragraph</p>
     </div>
-  )
-}
+  );
+};
 
-export default RegisterForm
+export default RegisterForm;
